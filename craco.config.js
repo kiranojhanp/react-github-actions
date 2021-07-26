f68bd9b302ff4dc5c817857aec7b938ee75b0e08
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2021-07-26 07:37:37 +0545

    Tailwind css added

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,15 @@
 const path = require(`path`);
 
 module.exports = {
-  webpack: {
-    alias: {
-      '@App': path.resolve(__dirname, 'src/'),
-      '@Components': path.resolve(__dirname, 'src/components'),
-    }
-  },
+	webpack: {
+		alias: {
+			"@App": path.resolve(__dirname, "src/"),
+			"@Components": path.resolve(__dirname, "src/components"),
+		},
+	},
+	style: {
+		postcss: {
+			plugins: [require("tailwindcss"), require("autoprefixer")],
+		},
+	},
 };
