de3ee7cfc021e71ebe9e900c9094f8018574b7ba
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2021-07-27 07:25:54 +0545

    first commit

--- src/index.js
+++ src/index.js
@@ -3,21 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import { Provider, Client, dedupExchange, fetchExchange } from "urql";
-import { cacheExchange } from "@urql/exchange-graphcache";
-
-const cache = cacheExchange({});
-
-const client = new Client({
-	url: "https://demo-graphql-yoga.glitch.me",
-	exchanges: [dedupExchange, cache, fetchExchange],
-});
-
 ReactDOM.render(
-	<Provider value={client}>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
-	</Provider>,
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
 	document.getElementById("root")
 );
